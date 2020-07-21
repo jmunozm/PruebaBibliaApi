399982d6ed7c2fe786d67d07cf3de7f8ed74c732
Juan Munoz <mmunoz.juan@gmail.com>
2020-07-21 09:11:47 -0500

    ya funciona find.json

--- src/Prueba1.java
+++ src/Prueba1.java
@@ -1,51 +1,73 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import jarroba.FootballPlayer;
+
 public class Prueba1 {
 	public static void main(String[] args) {
-		String url = "https://api.biblia.com/v1/bible/find.txt?key=fd37d8f28e95d3be8cb4fbc37e15e18e";
+		//String url = "https://api.biblia.com/v1/bible/find.txt?key=fd37d8f28e95d3be8cb4fbc37e15e18e";
+		Scanner fileIn = null;
 		String respuesta = "";
 		try {
-			respuesta = peticionHttpGet(url);
+			//respuesta = peticionHttpGet(url);
 			
-			JsonParser parser = new JsonParser();
-			// Obtain Array
-	        JsonArray gsonArr = parser.parse(respuesta).getAsJsonArray();
-	        // for each element of array
-	        for (JsonElement obj : gsonArr) {
+			fileIn = new Scanner(new FileReader("resources/json/find.json"));
+			
+			while(fileIn.hasNextLine()){
+				respuesta = fileIn.nextLine();
+			}
+			
+			Gson gson = new Gson();
+	        Biblias biblias = gson.fromJson(respuesta, Biblias.class);
 
-	            // Object of array
-	            JsonObject gsonObj = obj.getAsJsonObject();
-
-	            // Primitives elements of object
-	            String bible = gsonObj.get("bible").getAsString();
-	            String title = gsonObj.get("title").getAsString();
-	            String description = gsonObj.get("description").getAsString();
-
-	            // List of primitive elements
-	           // JsonArray demarcation = gsonObj.get("demarcation").getAsJsonArray();
-//	            List listDemarcation = new ArrayList();
-//	            for (JsonElement demarc : demarcation) {
-//	                listDemarcation.add(demarc.getAsString());
-//	            }
-
-	            // Object Constructor
-	            Biblia biblia = new Biblia();
-	            biblia.setBiblia(bible);
-	            biblia.setTitulo(title);
-	            biblia.setDescripcion(description);
-	            
-	            System.out.println(biblia);
+	        for (Biblia biblia : biblias.bibles) {
+	            System.out.println(biblia.bible);
+	            System.out.println(biblia.title);
+	            System.out.println(biblia.getDescription());
+	            System.out.println();
 	        }
+//			
+//			JsonParser parser = new JsonParser();
+//			// Obtain Array
+//	        JsonArray gsonArr = parser.parse(respuesta).getAsJsonArray();
+//	        // for each element of array
+//	        for (JsonElement obj : gsonArr) {
+//
+//	            // Object of array
+//	            JsonObject gsonObj = obj.getAsJsonObject();
+//
+//	            // Primitives elements of object
+//	            String bible = gsonObj.get("bible").getAsString();
+//	            String title = gsonObj.get("title").getAsString();
+//	            String description = gsonObj.get("description").getAsString();
+//
+//	            // List of primitive elements
+//	           // JsonArray demarcation = gsonObj.get("demarcation").getAsJsonArray();
+////	            List listDemarcation = new ArrayList();
+////	            for (JsonElement demarc : demarcation) {
+////	                listDemarcation.add(demarc.getAsString());
+////	            }
+//
+////	            // Object Constructor
+////	            Biblia biblia = new Biblia();
+////	            biblia.setBiblia(bible);
+////	            biblia.setTitulo(title);
+////	            biblia.setDescripcion(description);
+//	            
+////	            System.out.println(biblia);
+//	        }
 	        
 	        
 			//System.out.println("La respuesta es:\n" + respuesta);
